14cdfc79ed573e020c7bce791e584cd4fc2ea1e6
yvphaneendra9028 <mydestiny9028@gmail.com>
2026-07-29 19:57:15 +0530

    Update axios base URL to production environment

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
  const apiAxios = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://my-react-practice-code.onrender.com/api"
   withCredentials: true,
 });
 
